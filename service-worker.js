6b377960384e8d1dcb32aab005b030dbbb148e6b
Niharika Bommadi <bomma034@d.umn.edu>
2025-11-12 18:27:43 -0600

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.cb94730c5488230981ed6d1c32777209.js"
+  "/portfolio/precache-manifest.b2529ee330dae6671df2ae60fe96904c.js"
 );
 
 self.addEventListener('message', (event) => {
